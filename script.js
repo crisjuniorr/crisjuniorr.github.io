faed1c3776775e55be835f70b39e9f07850fea05
crisjuniorr <103531955+crisjuniorr@users.noreply.github.com>
2023-11-04 18:51:54 -0300

    Upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let odd = document.getElementById("odd")
+let valorDaAposta 
+let resultado = document.querySelector("span")
+
+
+function calcular() {
+  resultado.innerHTML= valorDaAposta = 300 / (parseFloat(odd.value) - 1)
+}
+
